deec8f1a29f69923eda2850f7125c8bf62fe96f4
Hadeev Marat <74759967+marat123123123@users.noreply.github.com>
2022-02-25 13:39:40 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+$('.header__burger').click(function(event){
+    $('.header__burger,.menu__body').toggleClass('active');
+    $('body').toggleClass('stop');
+});
+
+});
+
+
